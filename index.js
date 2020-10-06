15b01c899734e9fa2e7da33a88371482e40ca6c9
Moran1324 <67854443+Moran1324@users.noreply.github.com>
2020-10-06 14:47:36 +0300

    solved challenge

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
-class Declare{
-  //implement here
-}
+class Declare {
+  constructor(execute = () => {} ){
+    this.handlers = () => {};
+    this.state = 'PENDING';
+    this.value = null;
+    execute(this.resolve.bind(this));
+  };
+
+  resolve(value) {
+    if (this.state === 'PENDING') {
+      this.value = value;
+      this.state = 'FULFILLED';
+      this.handlers(value);
+    };
+  };
+
+  then(execute = () => {}){
+    this.handlers = execute;
+    if (this.state === 'FULFILLED') {
+      return new Declare((resolveExecute) => {
+        resolveExecute(execute(this.value))
+      });
+    } else {
+        return this;
+    }
+  };
+};
 module.exports = Declare
+
